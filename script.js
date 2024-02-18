093ce07a9f77acd87adf4129d993daa2b281b149
Ifham-Hashir <ifham09052003@gmail.com>
2024-02-18 10:22:26 +0530

    Add random bgcolor to the grids

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container");
 const sketchScreen = document.querySelector(".sketch-screen");
+const msg = prompt("Enter number of grids you want: ");
+const n = parseInt(msg);
 
 function createGrids(n) {
   for (let i = 1; i<= n; i++){
@@ -8,10 +10,29 @@ function createGrids(n) {
     for (let j = 1; j<= n; j++){
       row = document.createElement("div");
       row.classList.add("row");
-      row.style.border = "1px solid white";
+      row.style.border = "1px solid #D1D9E0";
       column.appendChild(row);
     }
     sketchScreen.appendChild(column);
   }
 }
-createGrids(16);
+createGrids(n);
+
+function randomColor() {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  return "rgb(" + x + "," + y + "," + z + ")";
+}
+
+let bgColor = document.querySelectorAll(".row");
+
+function changeBg(n) {
+  for (let i = 0; i < n*n; i++){
+    bgColor[i].addEventListener("mouseover", () => {
+      bgColor[i].style.background = randomColor();
+    });
+  }
+}
+
+changeBg(n);
